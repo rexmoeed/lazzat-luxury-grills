67e574b423840483cfdba4f5eecbba86a6b05163
rexmoeed <moeedmoody96@gmail.com>
2025-12-16 18:19:22 +0500

    Updated project files and added new images

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -687,7 +687,7 @@ const FilterDrawer = ({ open, onClose }: { open: boolean; onClose: () => void })
                 </select>
               </div>
 
-              <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6">
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6">
   {filteredSauces.map((sauce) => (
     <div
       key={sauce.name}
